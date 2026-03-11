c8d87a8a79fe056d5660cad20124cab1c1d43224
kritikabhagwani <zencoderlaksh@gmail.com>
2026-03-11 11:00:19 +0530

    dummy files and folders created backend

---
+++ backend/controllers/userController.js
@@ -0,0 +1,19 @@
+// Define user controller functions here
+
+const registerUser = (req, res) => {
+  res.send('Register User');
+};
+
+const loginUser = (req, res) => {
+  res.send('Login User');
+};
+
+const getMe = (req, res) => {
+  res.send('Get Me');
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getMe,
+};
---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+const protect = (req, res, next) => {
+  let token;
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(' ')[1];
+
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Get user from the token
+      req.user = decoded.id;
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401).json({ message: 'Not authorized, token failed' });
+    }
+  }
+
+  if (!token) {
+    res.status(401).json({ message: 'Not authorized, no token' });
+  }
+};
+
+module.exports = { protect };
